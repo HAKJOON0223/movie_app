f956990af6d509b75af99cee8d7990cd5d3f5f41
HAKJOON0223 <2hakjoon@gmail.com>
2020-06-12 20:24:53 +0900

    Updates

---
+++ precache-manifest.ea9ddce7c8394120c8047b50104cbed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48611695ba1b3490a923112300e878aa",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "407cec0f8350945b40e0",
+    "url": "/movie_app/static/css/main.eb65f846.chunk.css"
+  },
+  {
+    "revision": "043faffc56f8578dd9c6",
+    "url": "/movie_app/static/js/2.3ca0343f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.3ca0343f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407cec0f8350945b40e0",
+    "url": "/movie_app/static/js/main.b6868b47.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
